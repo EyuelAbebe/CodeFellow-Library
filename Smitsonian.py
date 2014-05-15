5faab0012e3675d20ff0bf97f6f20501d22a4e14
eyueld-bloc <eyueld-bloc@gmail.com>
2014-05-15 00:24:13 -0400

    Started the project

---
+++ Smitsonian.py
@@ -0,0 +1,28 @@
+
+
+welcome = """ 
+==================== Welcome to the Smithsonina Library ====================
+ 
+  We have books in the following subjects:
+          1 Mathematics
+          2 Physics
+          3 Chemistry
+          4 Biology
+          5 Engineering
+
+
+"""
+
+option = """ 
+  You can A) Check out a book from the Library.
+          B) Return a book.
+          C) Donate a book.
+          D) Browse
+
+"""
+
+selection = " Please pick an option: "
+
+print welcome
+print option
+print selection
